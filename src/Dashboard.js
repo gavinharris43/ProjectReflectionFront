35264a61b2a7dd8951457b29f89429d495ec60d8
bradstemitch <bradley.stephen.mitchell.95@gmail.com>
2019-02-19 11:49:06 +0000

    Dashboard Structure 1

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,14 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import './Dashboard.css';
 
 class Dashboard extends Component {
+
+
   render() {
+    let cohorts = [];
+    let trainees = [];
+    for (let i = 0; i < 12; i++) {
+      cohorts.push(
+        <button className="SideButton">Cohort: {i + 1}</button>
+      )
+    }
+    for (let i = 0; i < 100; i++) {
+      trainees.push(
+        <fieldset>
+          <legend>{i + 1}</legend>
+          <table className="TraineeTable">
+            <tbody>
+              <tr>
+                <td className="TraineeCardEntry">Personal Metrics</td>
+                <td className="TraineeCardEntry">Last Review</td>
+                <td className="TraineeCardEntry">Average Review</td>
+              </tr>
+            </tbody>
+          </table>
+        </fieldset>
+      )
+    }
     return (
-      <div>
-        <div className="MenuBar">
-          Hello
+      <div className="Dashboard">
+        <div className="SideBar">
+          Cohorts
+          {cohorts}
+          <button className="SideButton" id="AllAccountsButton">All Accounts</button>
+          <button className="SideButton" id="TrainersButton">Trainers</button>
+        </div>
+        <div className="TraineeList">
+          <header>Trainees</header>
+          Search
+          {trainees}
+        </div>
       </div>
-      </div>
+
     );
   }
 }
