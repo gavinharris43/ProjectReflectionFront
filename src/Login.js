b0023f21ffb004e196cb4d51e2d9cb424ebb9253
TomShawley <thomas.shawley1@gmail.com>
2019-02-19 12:13:04 +0000

    Basic login page

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-import './App.css';
+import './Login.css';
 import logo from './QA Consulting.png'
 
 class Login extends Component {
@@ -10,8 +10,18 @@ class Login extends Component {
         this.state = {
             email: '',
             password: ''
-        }
+        };
+        this.handleInputEmail = this.handleInputEmail.bind(this);
+        this.handleInputPassword = this.handleInputPassword.bind(this);
     }
+        handleInputEmail = (event) => {
+            this.setState({email:event.target.value});
+        }
+
+        handleInputPassword = (event) => {
+            this.setState({password:event.target.value});
+        }
+        
     render() {
         return (
             <div className = "Login">
@@ -19,9 +29,11 @@ class Login extends Component {
                     <img src={logo} className = "Logo"/>
                 </div> 
                 <div className = "Login-Form">
-                    <input type = "text" placeholder = "Email Address" value={this.state.email} onChange={(this.handleInputEmail)} required/>
+                    <input type = "text" id = "emailBox" placeholder = "Email Address" value={this.state.email} onChange={(this.handleInputEmail)} required/>
                     <br></br>
-                    <input type = "text" placeholder = "Password" value={this.state.password} onChange={(this.handleInputPassword)} required/>
+                    <input type = "text" id = "passwordBox" placeholder = "Password" value={this.state.password} onChange={(this.handleInputPassword)} required/>
+                    <br></br>
+                    <input type = "button" id = "loginButton" value = "Login" onClick={this.update} id = "Login-Button"></input>
                 </div>
             </div>
         );
