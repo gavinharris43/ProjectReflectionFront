fa0ca81df547bb0c916babcb4b1556d9e37acd33
bradstemitch <bradley.stephen.mitchell.95@gmail.com>
2019-02-19 09:42:14 +0000

    setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Dashboard from './Dashboard.js';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="">
+        <Dashboard />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="">
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
