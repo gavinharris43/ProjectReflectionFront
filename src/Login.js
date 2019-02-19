08eb38f9e6e7922559f07b7b424794e49185736a
TomShawley <thomas.shawley1@gmail.com>
2019-02-19 11:14:34 +0000

    Login form added

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
+import logo from './QA Consulting.png'
 
 class Login extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
     render() {
         return (
-            <div>
-                gsuighsd
+            <div className = "Login">
+                <div className = "Logo-Header">
+                    <img src={logo} className = "Logo"/>
+                </div> 
+                <div className = "Login-Form">
+                    <input type = "text" placeholder = "Email Address" value={this.state.email} onChange={(this.handleInputEmail)} required/>
+                    <br></br>
+                    <input type = "text" placeholder = "Password" value={this.state.password} onChange={(this.handleInputPassword)} required/>
+                </div>
             </div>
         );
     }
