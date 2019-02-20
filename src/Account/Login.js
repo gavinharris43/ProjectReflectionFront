c2f922563c17eb758deec53bf51f7898f11aa877
bradstemitch <bradley.stephen.mitchell.95@gmail.com>
2019-02-20 16:58:04 +0000

    css cleaned

---
+++ src/Account/Login.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import './Account.css';
+import logo from './QA Consulting.png'
+
+class Login extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: ''
+        };
+        this.handleInputEmail = this.handleInputEmail.bind(this);
+        this.handleInputPassword = this.handleInputPassword.bind(this);
+    }
+    handleInputEmail = (event) => {
+        this.setState({ email: event.target.value });
+    }
+
+    handleInputPassword = (event) => {
+        this.setState({ password: event.target.value });
+    }
+
+    render() {
+        return (
+            <div className="AccountPage">
+                <img className="AccountLogo" src={logo} />
+                <div className="AccountForm">
+                    <div className="InputBoxContainer">
+                        <input className="AccountInput" type="email" id="emailBox" placeholder="Email Address" required />
+                    </div>
+                    <div className="InputBoxContainer">
+                        <input className="AccountInput" type="password" id="passwordBox" placeholder="Password" required />
+                    </div>
+                </div>
+                <div>
+                    <button onClick={this.update} id="Login-Button">Login</button>
+                    <Link to="/register">
+                        <button onClick={this.update} id="Register-Button">Register</button>
+                    </Link>
+                </div>
+                <div>
+                    <Link to="/"><div>Forgot Password</div></Link>
+                    <Link to="/dashboard"><div>Dashboard for now</div></Link>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login;
--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import TrainerArea from './Dashboard/TrainerArea.js';
-import Login from './Login/Login.js';
-import Register from './Register/Register.js';
+import Login from './Account/Login.js';
+import Register from './Account/Register.js';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
 
@@ -10,14 +10,7 @@ class App extends Component {
     return (
       <BrowserRouter>
         <div>
-          <Route exact path='/' render={() => (
-            <div>
-              <Link to="/login"><div>Login</div></Link>
-              <Link to="/dashboard">Dashboard</Link>
-              <Link to="/register">Register</Link>
-            </div>
-          )} />
-          <Route path='/login' component={Login} />
+          <Route exact path='/' component={Login} />
           <Route path='/register' component={Register} />
           <Route path='/dashboard' component={TrainerArea} />
         </div>
